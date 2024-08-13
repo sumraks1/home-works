60519cad11fdac7080f8511a3f18967635c19bb7
sumraks1 <shulgavladislav1337@gmail.com>
2024-08-13 23:39:24 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = int(input('Введите число '))
+second = int(input('Введите число '))
+third = int(input('Введите число '))
+if first == second and second == third :
+    print(int("3"))
+elif first == second or second == third or first == third:
+    print(int("2"))
+elif first != second and second != third:
+    print(int("0"))
