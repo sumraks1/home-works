4e9280ebbced51117b570d45e8cbe132d9cfec46
sumraks1 <shulgavladislav1337@gmail.com>
2024-08-18 21:04:33 +0700

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,17 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while i < len(my_list):
-    number_in_list = int(input('Введите число из списка: '))
-    if number_in_list == my_list[i]:
+    if my_list[i] > 0:
         print(my_list[i])
         i += 1
-        if number_in_list <= 0:
-            break
+    else:
+        print(my_list[i + 1])
+        break
+
+
+
+
+
 
 
 
