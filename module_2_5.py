c3631f15f3fe46fe0a5741e98e26d2df039f67fc
sumraks1 <shulgavladislav1337@gmail.com>
2024-08-19 22:32:10 +0700

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,19 @@
+
+def get_matrix(n, m, volue):
+    matrix = []
+    for i in range(n):
+        str_matrix = []
+        for j in range(m):
+            str_matrix.append(volue)
+        matrix.append(str_matrix)
+    return matrix
+
+
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
