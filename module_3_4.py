e20f4d12595982b434f97aeca8cb09aae663f0ea
sumraks1 <shulgavladislav1337@gmail.com>
2024-08-29 20:50:44 +0700

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -3,8 +3,8 @@ def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
         root_word = root_word.lower()
-        i = i.lower()
-        if root_word in i or root_word.count(i):
+        k = i.lower()
+        if root_word in i or root_word.count(k):
             same_words.append(i)
 
     return same_words
